9d9d47634b1cf2522c8c85de50cdca0991f6b1c2
Cornel Izbasa <badc0ded@gmail.com>
2016-01-17 20:28:38 +0100

    Added Mandelbrot stuff.
    Signed-off-by: Cornel Izbasa <badc0ded@gmail.com>

--- initial/src/main/java/hello/Mandel.java
+++ initial/src/main/java/hello/Mandel.java
@@ -1,32 +1,78 @@
 package hello;
 
 public class Mandel {
+	private final int maxIter;
+	private int[] map;
+	private double y;
+	private double x;
+	private int dj;
+	private int di;
+	private double dxdi;
+	private double dydj;
 
-    private final double x;
-    private final double y;
-    private final double r;
-    private final long maxIter;
+	public Mandel(double x, double y, double dx, double dy, int di, int dj,
+			int maxIter) {
+		this.x = x;
+		this.y = y;
+		this.di = di;
+		this.dj = dj;
+		this.maxIter = maxIter;
+		this.dxdi = dx / di;
+		this.dydj = dy / dj;
+	}
 
-    public Mandel(double x, double y, double r, long maxIter) {
-    	this.x = x;
-    	this.y = y;
-    	this.r = r;
-    	this.maxIter = maxIter;
-    }
+	private int mand(double a, double b) {
+		int it;
+		double a1, a2;
+		double b1, b2;
+		for (it = 0, a1 = 0.0, b1 = 0.0; it < maxIter && a1 * a1 + b1 * b1 < 4; a2 = a1
+				* a1 - b1 * b1 + a, b2 = 2 * a1 * b1 + b, a1 = a2, b1 = b2, it++) {
+		}
+		if (it < maxIter) {
+			return it;
+		}
+		return 0;
+	}
 
-    public double getX() {
-        return x;
-    }
+	public void computeMap() {
+		int[] result = new int[di * dj];
+		for (int i = 0; i < di; i++) {
+			for (int j = 0; j < dj; j++) {
+				result[j * di + i] = mand(x + dxdi * i, y + dydj * j);
+			}
+		}
+		map = result;
+	}
 
-    public double getY() {
-        return y;
-    }
-    
-    public double getR() {
-    	return r;
-    }
-    
-    public long getMaxIter() {
-    	return maxIter;
-    }
+	public int[] getMap() {
+		return map;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public int getDi() {
+		return di;
+	}
+
+	public int getDj() {
+		return dj;
+	}
+
+	public int getMaxIter() {
+		return maxIter;
+	}
+
+	public double getDxdi() {
+		return dxdi;
+	}
+
+	public double getDydj() {
+		return dydj;
+	}
 }
--- initial/src/main/java/hello/MandelbrotController.java
+++ initial/src/main/java/hello/MandelbrotController.java
@@ -19,9 +19,14 @@ public class MandelbrotController {
     public Mandel mandel(
     			@RequestParam(value="x", defaultValue="-1.0") double x, 
     			@RequestParam(value="y", defaultValue="-1.0") double y,
-    			@RequestParam(value="r", defaultValue="2") double r,
-    			@RequestParam(value="maxIter", defaultValue="128") long maxIter
+    			@RequestParam(value="dx", defaultValue="2") double dx,
+    			@RequestParam(value="dy", defaultValue="2") double dy,
+    			@RequestParam(value="di", defaultValue="0") int di,
+    			@RequestParam(value="dj", defaultValue="0") int dj,
+    			@RequestParam(value="maxIter", defaultValue="128") int maxIter
     		) throws IOException {
-    	return new Mandel(x, y, r, maxIter);
+    	Mandel m = new Mandel(x, y, dx, dy, di, dj, maxIter);
+    	m.computeMap();
+    	return m;
     }
 }
